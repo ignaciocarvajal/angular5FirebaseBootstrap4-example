a1583902903b59e0fd9f7b15adffbda877d1a3a8
Ignacio Carvajal Duran <ignacio.carvajal@inacapmail.cl>
2018-02-14 01:07:17 -0300

    remove data firebase
    remove data

--- Angular5FireBaseBoottrap4/src/environments/environment.prod.ts
+++ Angular5FireBaseBoottrap4/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyCBnrdUVe-GX46CQiu4mrssO7hT5YnDmX0",
-    authDomain: "angularfirebaseexample-2ab2f.firebaseapp.com",
-    databaseURL: "https://angularfirebaseexample-2ab2f.firebaseio.com",
-    projectId: "angularfirebaseexample-2ab2f",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
     storageBucket: "",
-    messagingSenderId: "60351955833"
+    messagingSenderId: ""
   }
 };
